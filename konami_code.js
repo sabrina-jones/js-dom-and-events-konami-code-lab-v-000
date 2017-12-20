04d4168811e3594743ca40885bdc6f9625f89a74
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-20 15:24:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,3 +3,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
    document.body.addEventListener('keydown',keyDownHandler)
 }
+
+function keyDownHandler(event){
+  const key = parseInt(event.detail || event.which)
+
+  if (key===code[index]){
+    index++
+
+    if (index===code.length-1){
+      alert('Congrat!')
+      index=0
+    }
+  } else {
+    index=0
+  }
+}
